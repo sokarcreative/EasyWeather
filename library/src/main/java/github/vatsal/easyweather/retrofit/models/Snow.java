56d4d874ad561dacc9b01277fcfc57642eac3bb3
sokarcreative <marinx.denis@gmail.com>
2017-04-13 12:05:42 +0200

    FIx CurrentWeather (OpenWeatherApi changed) Add weather icons

---
+++ library/src/main/java/github/vatsal/easyweather/retrofit/models/Snow.java
@@ -0,0 +1,36 @@
+package github.vatsal.easyweather.retrofit.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by
+ * --Vatsal Bajpai under
+ * --AppyWare on
+ * --23/06/16 at
+ * --1:50 AM in
+ * --OpenWeatherMapDemo
+ */
+
+public class Snow {
+
+    @SerializedName("3h")
+    @Expose
+    private Double _3h;
+
+    public Double get3h() {
+        return _3h;
+    }
+
+    public void set3h(Double _3h) {
+        this._3h = _3h;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Snow{" +
+                "_3h=" + _3h +
+                '}';
+    }
+}
